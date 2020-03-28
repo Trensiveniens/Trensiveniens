094d0f2676b00e973711083ad7554f1071b63d27
Icad <irsyadmuhammad.020201@gmail.com>
2020-03-28 14:48:02 +0700

    Major update, 28/03/2020, ver 0.0.2

--- myScript.js
+++ myScript.js
@@ -1,69 +1,86 @@
 //SLIDER
 
-$(document).ready(function(){
-    // Initialize Tooltip
-    $('[data-toggle="tooltip"]').tooltip(); 
-    
-    // Add smooth scrolling to all links in navbar + footer link
-    $(".navbar a, footer a[href='#myPage']").on('click', function(event) {
-  
-      // Make sure this.hash has a value before overriding default behavior
-      if (this.hash !== "") {
-  
-        // Prevent default anchor click behavior
-        event.preventDefault();
-  
-        // Store hash
-        var hash = this.hash;
-  
-        // Using jQuery's animate() method to add smooth page scroll
-        // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
-        $('html, body').animate({
-          scrollTop: $(hash).offset().top
-        }, 900, function(){
-     
-          // Add hash (#) to URL when done scrolling (default click behavior)
-          window.location.hash = hash;
-        });
-      } // End if
-    });
-  })
-  
-  //CHANGE TEXT
-  // function for change text lihat semua to lihat lebih sedikit
-  function changeText(){
+$(document).ready(function () {
+  // Initialize Tooltip
+  $('[data-toggle="tooltip"]').tooltip();
+
+  // Add smooth scrolling to all links in navbar + footer link
+  $(".navbar a, footer a[href='#myPage']").on('click', function (event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (900) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 900, function () {
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+    } // End if
+  });
+})
+
+//CHANGE TEXT
+// function for change text lihat semua to lihat lebih sedikit
+function changeText() {
   var p = document.getElementById('viewMore').innerHTML;
-    if (p == 'Lihat Semua') {
-      document.getElementById('viewMore').innerHTML = 'Lihat Lebih Sedikit';
-    }
-    else{
-     document.getElementById('viewMore').innerHTML = 'Lihat Semua';
-    }
+  if (p == 'Lihat Semua') {
+    document.getElementById('viewMore').innerHTML = 'Lihat Lebih Sedikit';
   }
-  
-  // LOGIN PAGE
-  // Get the modal
-  var modal = document.getElementById("myModal");
-  
-  // Get the button that opens the modal
-  var btn = document.getElementById("myBtn");
-  
-  // Get the <span> element that closes the modal
-  var span = document.getElementsByClassName("close")[0];
-  
-  // When the user clicks on the button, open the modal
-  btn.onclick = function() {
-    modal.style.display = "block";
+  else {
+    document.getElementById('viewMore').innerHTML = 'Lihat Semua';
   }
-  
-  // When the user clicks on <span> (x), close the modal
-  span.onclick = function() {
+}
+
+// LOGIN PAGE
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function () {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function () {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
   }
-  
-  // When the user clicks anywhere outside of the modal, close it
-  window.onclick = function(event) {
-    if (event.target == modal) {
-      modal.style.display = "none";
+}
+
+//collapse
+
+var acc = document.getElementsByClassName("accordion");
+var is;
+
+for (is = 0; is < acc.length; is++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight ="22px";
     }
-  }
+  });
+}
